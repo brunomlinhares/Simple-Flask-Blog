f0c9d7fc44fd210b44210fe3cdf031b5c0f2fdbd
Bruno Linhares <brunomatheuslinhares2@gmail.com>
2023-07-22 09:48:20 -0300

    Changed the scope of config app upload folder

--- blog_app/config.py
+++ blog_app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    UPLOAD_FOLDER = os.path.join("static", "upload")
+    UPLOAD_FOLDER = os.path.join("blog_app","static", "upload")
     SECRET_KEY="dev",
     MAX_CONTENT_LENGTH = 1024 * 1024
     ALLOWED_IMAGE_EXTENSIONS = ["png", "jpg", "jpeg", "gif"]
