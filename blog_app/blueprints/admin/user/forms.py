46501e19434437df876678d3d9763f44752b9aca
Bruno Linhares <brunomatheuslinhares2@gmail.com>
2023-07-22 09:48:48 -0300

    Added user profile image to blueprints

--- blog_app/blueprints/admin/user/forms.py
+++ blog_app/blueprints/admin/user/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField,SubmitField,validators
+from wtforms import StringField, BooleanField,SubmitField,validators,FileField
 class UserRegisterAdminForm(FlaskForm):
         name = StringField("Name", [validators.DataRequired(message="Este campo é obrigatório")],name="name")
         email = StringField("Email", [validators.DataRequired()],name="email")
+        profile_image = FileField("Profile Image")
         is_admin = BooleanField("Is Admin")
         submit = SubmitField("Submit")
         password = StringField("Password", [validators.DataRequired()],name="password")
--- blog_app/blueprints/admin/user/user.py
+++ blog_app/blueprints/admin/user/user.py
@@ -15,23 +15,52 @@ from flask_login import login_required
 from blog_app.models.User import User
 from blog_app import db
 
+from blog_app.use_cases.user.query_user import get_all_users
+from blog_app.use_cases.user.delete_user import delete_user
+from blog_app.use_cases.user.update_user import update_user,update_user_image
+from blog_app.use_cases.user.create_user import create_user
+from blog_app.use_cases.user.query_user import get_user_by_id
+import io
+
+
 @user_bp.route("/",methods=["GET","POST"])
 @login_required
 def index():
     form = UserRegisterAdminForm()
     if form.validate_on_submit():
-        user = User(
+        profile_image = request.files.get("profile_image")
+        if profile_image:
+            user = create_user(
+                name=form.name.data,
+                email=form.email.data,
+                password=form.password.data,
+                image_bytes=io.BytesIO(profile_image.read())
+            )
+    users = get_all_users()
+    return render_template("admin/user/index.html", form=form, users=users,page_title="User List")
+
+
+@user_bp.route("/<int:user_id>/",methods=["GET","POST"])
+@login_required
+def edit(user_id):
+    user = get_user_by_id(user_id)
+    form = UserRegisterAdminForm(obj=user)
+    if form.validate_on_submit():
+        profile_image = request.files.get("profile_image")
+        user = update_user(
+            user=user,
             name=form.name.data,
             email=form.email.data,
             password=form.password.data,
-           
         )
-        db.session.add(user)
-        db.session.commit()
-    users = User.query.all()
-  
+        if profile_image:
+            update_user_image(user=user,image_bytes=io.BytesIO(profile_image.read()))
+    return render_template("admin/user/edit.html", form=form, user=user,page_title="Edit User")
 
 
-
-    
-    return render_template("admin/user/index.html", form=form, users=users,page_title="User List")
+@user_bp.route("/<int:user_id>/delete",methods=["GET","POST"])
+@login_required
+def delete(user_id):
+    user = get_user_by_id(user_id)
+    delete_user(user)
+    return render_template("admin/user/delete.html", user=user,page_title="Delete User")
